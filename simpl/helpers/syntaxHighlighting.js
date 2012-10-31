aeaed02d1427f5631860de35813d94b1f7569f91
Nic <nic@ecologylab.net>
2012-10-31 17:17:09 -0500

    simpl type scope rendering

--- simpl/helpers/syntaxHighlighting.js
+++ simpl/helpers/syntaxHighlighting.js
@@ -33,3 +33,14 @@ HTMLDivElement.prototype.killChildren = function()
 	}
 }
 
+function clone(obj){
+    if(obj == null || typeof(obj) != 'object')
+        return obj;
+
+    var temp = obj.constructor(); // changed
+
+    for(var key in obj)
+        temp[key] = clone(obj[key]);
+    return temp;
+}
+
