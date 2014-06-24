3216faf9df59791a0d8d030842bbc869966281a5
unknown <kchan@oberlin.edugit config --global user.email kchan@oberlin.edugit config --global user.email kchan@oberlin.edu>
2014-06-24 16:58:19 -0500

    added scalar info of the page on to the slide out

--- plugin/code/SlideOut.js
+++ plugin/code/SlideOut.js
@@ -1,4 +1,4 @@
-/*
+/**
  * Creates the slide-out to display information about the meta-metadata, metadata, and clippings available on the page.
  */
 function buildSlideOut(document)
@@ -11,10 +11,108 @@ function buildSlideOut(document)
 	
 	// add new div to the page
 	document.body.appendChild(slideOutVisual);
-	console.log("built slideout visual");
 }
 
-/*
+
+/**
+ * Display the given meta-metadata in the slideout so that user can see it
+ * @param mmd, meta-metadata json object
+ */
+function renderMMD(mmd)
+{
+	//var m = document.createElement("p");
+	
+	var typeHeader = document.createElement('h1');
+		typeHeader.className = "mmdTypeTitle";
+	
+		typeHeader.innerText = prettifyText(mmd["name"]);
+		
+	slideOutVisual.appendChild(typeHeader);
+	
+	var kids = mmd['kids'];
+	
+	var child;
+	var k; //string of stuff for kids
+	
+	for (var i = 0; i < kids.length; i++)
+	{
+		var field = kids[i];
+		//console.log(field.scalar);
+		
+		// render scalar types
+		if(field.scalar != null)
+		{
+			field = field.scalar;
+			//console.log(field);
+			
+			var mmdField = document.createElement('div');
+				mmdField.className = "mmdField";
+				//dont need prettifytext but if its not there undefinds show up
+				mmdField.innerText = prettifyText(field["name"]); 
+				
+			var mmdFieldType = document.createElement('span');
+				mmdFieldType.className = "mmdFieldType";
+			
+				mmdFieldType.innerText = " : " + prettifyText(field["scalar_type"]);
+				
+			mmdField.appendChild(mmdFieldType);
+				
+			slideOutVisual.appendChild(mmdField);
+		}
+		/*
+		
+		child = kids[i]['scalar'];
+		k += "<p>kids[" + i + "]: " + "</p>";
+		try {
+			console.log(child['name']);
+			k += "<p>name: " + child['name'] + "</p>" ;
+		} catch(e) {
+			console.log('qwer');
+		}
+		try {
+			console.log(child['scalar']);
+			k += "<p>scalar type: " + (typeof child['scalar']) + "</p>";
+		} catch(e){
+			console.log('asdf');
+		}
+		//k += "<br>";
+		*/
+	}
+	
+	//var str = printout(mmd);
+	//var str;
+	
+	//str = "<p><b>type: " + mmd["name"] + "</b></p>";
+	//str += "kids: " + mmd['kids'] + " ";
+	//str += "<p>kids: </p>" + k;
+
+	
+	//var t = document.createTextNode(str);
+	//m.appendChild(t);
+	
+	
+	// make it display pretty
+	
+	// dynamically build html to render the mmd
+	
+	// iterate through the mmd to find all the fields
+	
+	// style it cool and legible
+	
+	//slideOutVisual.innerHTML = str + k;
+	
+}
+
+function prettifyText(str)
+{
+	// replaces '_' with spaces
+	str = str.replace('_', " ");
+	
+	return str;
+}
+
+
+/**
  * Toogles the Slide-Out, by sliding it in or out of view
  */ 
 function toggleSlideOut()
@@ -35,7 +133,7 @@ function toggleSlideOut()
 	}	
 }
 
-/*
+/**
  * slides the slideout out
  */
 function goOut() 
@@ -44,7 +142,7 @@ function goOut()
 	slideOutVisual.style.boxShadow = "6px 0 16px 5px #888";
 }
 
-/*
+/**
  * slides the slideout back in
  */
 function goIn()
---
+++ plugin/code/utilites.js
@@ -0,0 +1,10 @@
+var Utilities = {};
+
+/*
+Utilities.doJSONPCall = function(jsonpURL)
+{
+  var script = document.createElement('script');
+  script.src = jsonpURL;
+  document.head.appendChild(script);
+}
+*/
