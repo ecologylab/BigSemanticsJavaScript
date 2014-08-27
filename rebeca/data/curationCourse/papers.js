1131d99ed9a359b98fed82e2b22ef6df068e40b3
Nic Lupfergit config --global user.email nic@ecologylab.netgit config --liargit config --global user.name Nic <nic@ecologylab.net>
2014-08-27 16:18:41 -0500

    this is absolutely nothing, you will ignore this commit if you know what's good for you

---
+++ rebeca/data/curationCourse/papers.js
@@ -0,0 +1,105 @@
+var CURATION_PAPERS = [
+{
+	title: "Curation is Creation",
+	location: "http://colin.io/portfolio/curation-is-creation/",
+	authors: [
+		"Wright, Colin"
+		],
+	year: 2014,
+	set: "#1 - 9/4 Introduction",
+	publisher: "Asymmetrical Press"
+},
+
+{
+	title: "Using Metrics of Curation to Evaluate Information-based Ideation",
+	location: "http://ecologylab.net/research/publications/a14-kerneInfoBasedIdeationToCHI.pdf",
+	authors: [
+		"Kerne, Andruid",
+		"Webb, Andrew",
+		"Smith, Steven",
+		"Linder, Rhema",
+		"Lupfer, Nic",
+		"Qu, Yin",
+		"Moeller, Jon",
+		"Damaraju, Sashi"
+		],
+	year: 2014,
+	set: "#3 - 9/11 Information-based Ideation and Curation",
+	publisher: "ToCHI"
+},
+
+{
+	title: "The Culture of Curating and the Curating of Culture(s)",
+	location: "",
+	authors: [
+		"O'Neill, Patrick"
+		],
+	year: 2012,
+	set: "#4 - 9/16 Curation 1",
+	publisher: "MIT Press"
+},
+
+{
+	title: "The Culture of Curating and the Curating of Culture(s)",
+	location: "",
+	authors: [
+		"O'Neill, Patrick"
+		],
+	year: 2012,
+	set: "#5 - 9/18 Curation 2",
+	publisher: "MIT Press"
+},
+
+{
+	title: "The Art of Assemblage",
+	location: "",
+	authors: [
+		"Seitz, William"
+		],
+	year: 1961,
+	set: "#6 - 9/23 Assemblage",
+	publisher: "MOMA"
+},
+
+{
+	title: "Similarity and emergence in conceptual combination",
+	location: "http://personal.stevens.edu/~ysakamot/creativity/similarity%20emergence.pdf",
+	authors: [
+		"Wilkenfeld, Merryl",
+		"Ward, Thomas"
+		],
+	year: 2001,
+	set: "#7 - 9/25 Emergence and Analogy",
+	publisher: "Journal of Memory and Language"
+},
+
+{
+	title: "Structure Mapping: A Theoretical Framework for Analogy",
+	location: "http://csjarchive.cogsci.rpi.edu/1983v07/i02/p0155p0170/MAIN.PDF",
+	authors: [
+		"Gentner, Dedre"
+		],
+	year: 1983,
+	set: "#7 - 9/25 Emergence and Analogy",
+	publisher: "Cognitive Science"
+}
+
+
+
+];
+
+
+/*
+
+{
+	title: "",
+	location: "",
+	authors: [
+		""
+		],
+	year: 20,
+	set: "",
+	publisher: ""
+},
+
+ */
