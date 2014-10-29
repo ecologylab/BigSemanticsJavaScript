b8e23f1bd3b1502296cc2b2509c03ce263cb1064
Kade Keith <keithkade@aol.com>
2014-10-29 17:49:39 -0500

    style, courtesy of jshint

--- mona/js/mona.js
+++ mona/js/mona.js
@@ -1,6 +1,8 @@
+/*global waitForNewMMD, MDC_rawMMD, getNodes, allNodeMDLoaded, document, setTimeout, MetadataLoader, console, hexToRgb, FlairMaster, sortNumber, median, MDC_rawMetadata, showMetadata*/
+
 var MONA = {};
 var cachedMMD = "";
-cachedNodeMetadata = {};
+var cachedNodeMetadata = {};
 var nodeColors = {};
 var nodes = {};
 var nodeMetadata = {};
@@ -12,18 +14,17 @@ var tier4size = 10;
 var tier3size = 20;
 var tier2size = 30;
 var tier1size = 40;
-var colorArray = ["#009933","#006699","#CC9900","#CC0000","#CC00CC"]
+var colorArray = ["#009933", "#006699", "#CC9900", "#CC0000", "#CC00CC"];
 
-function Node (type, title, location, mmdName){
+function Node(type, title, location, mmdName){
 	this.type = type;
 	this.title = title;
-	this.abbrevTitle = title.substring(0,29) + "...";
+	this.abbrevTitle = title.substring(0, 29) + "...";
 	this.location = location;
 	this.mmdName = mmdName;
-} 
+}
 
-MONA.initialize = function(){
-	//cachedMMD = "";
+MONA.initialize = function (){
 	cachedNodeMetadata = {};
 	nodeColors = {};
 	nodes = {};
@@ -32,51 +33,54 @@ MONA.initialize = function(){
 	typePositions = {};
 	colorCount = 0;
 	requestsMade = 0;
-	var graphElement = document.getElementById("graphArea");
-	while (graphElement.firstChild) {
-    	graphElement.removeChild(graphElement.firstChild);
+	var graphElement = document.getElementById("graphArea"),
+        typeElement = document.getElementById("typeArea"),
+        linesElement = document.getElementById("lineSVG"),
+        loadingElement = document.getElementById("nodeMDArea"),
+        nodesLoading = document.createElement('div'),
+        nodeMDLoading = document.createElement('div');
+	
+    while (graphElement.firstChild){
+        graphElement.removeChild(graphElement.firstChild);
 	}
-	var typeElement = document.getElementById("typeArea");	
-	while (typeElement.firstChild) {
-    	typeElement.removeChild(typeElement.firstChild);
+	while (typeElement.firstChild){
+        typeElement.removeChild(typeElement.firstChild);
 	}
-	var linesElement = document.getElementById("lineSVG");	
-	while (linesElement.firstChild) {
-    	linesElement.removeChild(linesElement.firstChild);
+	while (linesElement.firstChild){
+        linesElement.removeChild(linesElement.firstChild);
 	}
-	var nodesLoading = document.createElement('div');
+    
 	nodesLoading.innerHTML = "Loading Nodes...";
 	graphElement.appendChild(nodesLoading);
 
-	var loadingElement = document.getElementById("nodeMDArea");
-	while (loadingElement.firstChild) {
-    	loadingElement.removeChild(loadingElement.firstChild);
+	while (loadingElement.firstChild){
+        loadingElement.removeChild(loadingElement.firstChild);
 	}
-	var nodeMDLoading = document.createElement('div');
-	nodeMDLoading.innerHTML = "Loading Nodes Metadata...";
+
+    nodeMDLoading.innerHTML = "Loading Nodes Metadata...";
 	loadingElement.appendChild(nodeMDLoading);
 	waitForNewMMD();
-}
+};
 
 //waits until the new metadata comes in before updating the nodes
-function waitForNewMMD() {
-    if(MDC_rawMMD===cachedMMD) {
+function waitForNewMMD(){
+    if (MDC_rawMMD === cachedMMD){
         setTimeout(waitForNewMMD, 100);
         return;
     }
-    cachedMMD=MDC_rawMMD;
+    cachedMMD = MDC_rawMMD;
     var graphElement = document.getElementById("graphArea");
-	while (graphElement.firstChild) {
-    	graphElement.removeChild(graphElement.firstChild);
+	while (graphElement.firstChild){
+        graphElement.removeChild(graphElement.firstChild);
 	}
     getNodes();
 }
 
-function waitForNodeMDLoaded() {
+function waitForNodeMDLoaded(){
 	//if url is 404, then we get an http error 500 from the service and get stuck
 	//there are some other cases where we get a service error and get stuch
 	//make it greater than one to account for not getting 404s
-    if(Object.keys(nodeMetadata).length == 0 || requestsMade - (Object.keys(nodeMetadata).length) > 1) {
+    if (Object.keys(nodeMetadata).length === 0 || requestsMade - (Object.keys(nodeMetadata).length) > 1) {
         setTimeout(waitForNodeMDLoaded, 100);
         return;
     }
@@ -85,11 +89,11 @@ function waitForNodeMDLoaded() {
 
 function populateNodeMetadata(){
 	//for now don't try to load mmd if there is more than 30 nodes
-	if (Object.keys(nodes).length > 20){
+	if (Object.keys(nodes).length > 20) {
 		return;
 	}
-	for (nodeKey in nodes){
-		if (nodes[nodeKey].location != undefined){
+	for (var nodeKey in nodes) {
+		if (nodes[nodeKey].location !== undefined) {
     		MetadataLoader.getMetadata(nodes[nodeKey].location, "storeNodeMD", false);
 			requestsMade++;
 		}
@@ -100,11 +104,11 @@ function populateNodeMetadata(){
 
 function storeNodeMD(rawMetadata, requestMmd){
 	//innifecient could be improved
-	for (key in rawMetadata){
-		console.log("got some metadata for " + rawMetadata[key]['title']);
-		for (nodeKey in nodes){
+	for (var key in rawMetadata){
+		console.log("got some metadata for " + rawMetadata[key].title);
+		for (var nodeKey in nodes){
 			//kinda sloppy way to handle redirects
-			if (nodes[nodeKey].location != undefined && (nodes[nodeKey].location.indexOf(rawMetadata[key].location) > -1 || rawMetadata[key].location.indexOf(nodes[nodeKey].location) > -1)){
+			if (nodes[nodeKey].location !== undefined && (nodes[nodeKey].location.indexOf(rawMetadata[key].location) > -1 || rawMetadata[key].location.indexOf(nodes[nodeKey].location) > -1)){
 				//show node as loaded
 				nodeMetadata[nodeKey] = rawMetadata;
 				nodeMDLoaded(nodeKey);
@@ -123,13 +127,13 @@ function nodeMDLoaded(nodeKey){
 	//if incoming metadata is of a different type than we expected update the image
 	//FIXME not sure if it works
 	if (!nodeMetadata[nodeKey].hasOwnProperty(nodes[nodeKey].mmdName)){
-		for (newMMDName in nodeMetadata[nodeKey]){
+		for (var newMMDName in nodeMetadata[nodeKey]){
 			var img = FlairMaster.getFlairImage(newMMDName).cloneNode(true);
 			img.setAttribute('height',tier3size+'px');
 			img.setAttribute('width',tier3size+'px');
 			
 			var curImgArray = div.getElementsByTagName('img');
-			curImg = curImgArray[0];
+			var curImg = curImgArray[0];
 			curImg = img;
 		}
 	}
@@ -139,6 +143,7 @@ function nodeMDLoaded(nodeKey){
 }
 
 function allNodeMDLoaded(){
+    "use strict";
 	var loadingElement = document.getElementById("nodeMDArea");
 	while (loadingElement.firstChild) {
     	loadingElement.removeChild(loadingElement.firstChild);
@@ -150,18 +155,18 @@ function allNodeMDLoaded(){
 
 
 function updateImgSizes(mmdType){
-	
+    "use strict";
 	var citationCountList = [];
 	var foundOne = false;
-	for (nodeKey in nodeMetadata){
-		if (nodeMetadata[nodeKey][mmdType] != null){
+	for (var nodeKey in nodeMetadata){
+		if (nodeMetadata[nodeKey][mmdType] !== null){
 			foundOne = true;
-			if (mmdType == "acm_portal" && nodeMetadata[nodeKey].acm_portal.citations != null){
+			if (mmdType == "acm_portal" && nodeMetadata[nodeKey].acm_portal.citations !== null){
 				citationCountList.push(nodeMetadata[nodeKey].acm_portal.citations.length);
 			}
 			//currently ranks by total citations
 			//up for debate if this is best
-			else if (mmdType == "acm_portal_author" && nodeMetadata[nodeKey].acm_portal_author.publication_detail.citation_count != null){
+			else if (mmdType == "acm_portal_author" && nodeMetadata[nodeKey].acm_portal_author.publication_detail.citation_count !== null){
 				citationCountList.push(parseInt(nodeMetadata[nodeKey].acm_portal_author.publication_detail.citation_count));
 			}
 			else { 
@@ -182,15 +187,15 @@ function updateImgSizes(mmdType){
 	var bottomMedian = median(rightSide);
 		
 	for (nodeKey in nodes){
-		if (nodeMetadata.hasOwnProperty(nodeKey) && nodeMetadata[nodeKey][mmdType] != null){
+		if (nodeMetadata.hasOwnProperty(nodeKey) && nodeMetadata[nodeKey][mmdType] !== null) {
 			var citationCount = 0;
 			if (mmdType == "acm_portal"){
-				if (nodeMetadata[nodeKey].acm_portal.citations != null){
+				if (nodeMetadata[nodeKey].acm_portal.citations !== null) {
 					citationCount = nodeMetadata[nodeKey].acm_portal.citations.length;
 				}
 			}
 			else if (mmdType == "acm_portal_author"){
-				if (nodeMetadata[nodeKey].acm_portal_author.publication_detail.citation_count != null){
+				if (nodeMetadata[nodeKey].acm_portal_author.publication_detail.citation_count !== null){
 					citationCount = nodeMetadata[nodeKey].acm_portal_author.publication_detail.citation_count;
 				}
 			}
@@ -217,33 +222,34 @@ function updateImgSizes(mmdType){
 }
 
 function getNodes(){
+    "use strict";
 	console.log(MDC_rawMetadata);
 	console.log(MDC_rawMMD);
-	for (metadataType in MDC_rawMetadata){
+	for (var metadataType in MDC_rawMetadata){
 		
-		for (key in MDC_rawMetadata[metadataType]){
-			
+		for (var key in MDC_rawMetadata[metadataType]){
+            var newNode;
 			var currentField = MDC_rawMetadata[metadataType][key];
 			if (currentField instanceof Array){
 				if (currentField[0].hasOwnProperty("meta_metadata_name")){
-					if (currentField[0]["meta_metadata_name"] != "rich_document" && currentField[0]["meta_metadata_name"] != "image"){
+					if (currentField[0].meta_metadata_name != "rich_document" && currentField[0].meta_metadata_name != "image"){
 						nodeColors[key] = colorArray[colorCount];
 						colorCount++;
 						for (var i = 0;  i < currentField.length; i++){
-							var newNode = new Node(key, currentField[i]["title"], currentField[i]["location"], currentField[i]["meta_metadata_name"]);
-							nodes[currentField[i]["title"]] = newNode;
+							newNode = new Node(key, currentField[i].title, currentField[i].location, currentField[i].meta_metadata_name);
+							nodes[currentField[i].title] = newNode;
 						}
 					}
 				}
 				else {
-					for (key2 in currentField[0]){
+					for (var key2 in currentField[0]){
 						if (currentField[0][key2].hasOwnProperty("meta_metadata_name")){
-							if (currentField[0][key2]["meta_metadata_name"] != "rich_document" && currentField[0][key2]["meta_metadata_name"] != "image"){
+							if (currentField[0][key2].meta_metadata_name != "rich_document" && currentField[0][key2].meta_metadata_name != "image"){
 								nodeColors[key] = colorArray[colorCount];
 								colorCount++;				
-								for (var i = 0;  i < currentField.length; i++){
-									var newNode = new Node(key, currentField[i][key2]["title"], currentField[i][key2]["location"], currentField[i][key2]["meta_metadata_name"]);
-									nodes[currentField[i][key2]["title"]] = newNode;
+								for (var j = 0;  j < currentField.length; j++){
+									newNode = new Node(key, currentField[j][key2].title, currentField[j][key2].location, currentField[j][key2].meta_metadata_name);
+									nodes[currentField[j][key2].title] = newNode;
 								}			
 							}
 						}
@@ -252,11 +258,11 @@ function getNodes(){
 			}
 			if (currentField instanceof Object){
 				if (currentField.hasOwnProperty("meta_metadata_name") && currentField.hasOwnProperty("location")){
-					if (currentField["meta_metadata_name"] != "rich_document" && currentField["meta_metadata_name"] != "image"){
+					if (currentField.meta_metadata_name != "rich_document" && currentField.meta_metadata_name != "image"){
 						nodeColors[key] = colorArray[colorCount];
 						colorCount++;
-						var newNode = new Node(key, currentField["title"], currentField["location"], currentField["meta_metadata_name"]);
-						nodes[currentField["title"]] = newNode;
+						newNode = new Node(key, currentField.title, currentField.location, currentField.meta_metadata_name);
+						nodes[currentField.title] = newNode;
 					}
 				}
 			}
@@ -266,7 +272,7 @@ function getNodes(){
 	drawNodes();
 	drawTypes();
 	drawLines();
-};
+}
 
 function onNodeClick(location){
 	document.getElementById("targetURL").value = location;
@@ -319,11 +325,11 @@ function onTypeMouseout(type){
 }
 
 function drawNodes(){
-	for (nodeKey in nodes){
+	for (var nodeKey in nodes){
 		var graphElement = document.getElementById("graphArea");
 		var div = document.createElement('div');
 		
-		if (nodes[nodeKey].location != undefined){//visualize this
+		if (nodes[nodeKey].location !== undefined){//visualize this
 			div.style.cursor = "pointer";
 			div.setAttribute('onclick','onNodeClick("'+nodes[nodeKey].location+'")');
 		}
@@ -331,7 +337,7 @@ function drawNodes(){
 		div.setAttribute('onmouseout','onNodeMouseout("'+nodeKey+'")');
 		div.id = nodeKey;
 		
-		var nodeText = ""
+		var nodeText = "";
 		if(nodeKey.length > 30)
 			nodeText = nodes[nodeKey].abbrevTitle;
 		else
@@ -340,7 +346,7 @@ function drawNodes(){
 		nodePara.innerHTML = nodeText;
 		nodePara.className = "nodeText";
 		
-		for (nodeType in nodeColors)
+		for (var nodeType in nodeColors)
 			if (nodes[nodeKey].type == nodeType){
 				div.className=nodeType;
 				var rgb = hexToRgb(nodeColors[nodeType]);
@@ -361,7 +367,7 @@ function drawNodes(){
 
 function drawTypes(){
 	var typeElement = document.getElementById("typeArea");
-	for (nodeType in nodeColors){
+	for (var nodeType in nodeColors){
 		var div = document.createElement('div');
 		div.setAttribute('onmouseover','onTypeMouseover("'+nodeType+'")');
 		div.setAttribute('onmouseout','onTypeMouseout("'+nodeType+'")');
@@ -380,9 +386,9 @@ function drawTypes(){
 
 }
 
-function drawLines(){	
+function drawLines(){
 	var typeElement = document.getElementById("lineSVG");
-	for (nodeKey in nodes){
+	for (var nodeKey in nodes){
 		var line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
 		line.setAttribute('class', nodes[nodeKey].type+"Line");
 		line.setAttribute('id', nodes[nodeKey].title+"Line");
@@ -398,7 +404,7 @@ function drawLines(){
 }
 
 function updateLines(){
-	for (nodeKey in nodes){
+	for (var nodeKey in nodes){
 		//update line ends
 		var div = document.getElementById(nodeKey);
 		nodePositions[nodeKey] = div.getBoundingClientRect();
