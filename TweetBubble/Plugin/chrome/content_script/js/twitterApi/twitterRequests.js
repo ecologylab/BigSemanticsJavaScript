fa439a36abb31c56ea235ba33d3d8e9e43556993
Ajit Jain <ajit.infotech@gmail.com>
2015-01-28 17:10:01 -0600

    refactor

--- TweetBubble/Plugin/chrome/content_script/js/twitterApi/twitterRequests.js
+++ TweetBubble/Plugin/chrome/content_script/js/twitterApi/twitterRequests.js
@@ -11,21 +11,8 @@ TwitterRequests.postReply = function(tweetStr)
 			{
 				console.log("***post tweet***: " + tweetStr);
 				
-				var xhr = new XMLHttpRequest();
 				var url = "https://api.twitter.com/1.1/statuses/update.json";
 				var body = "status=" + encodeURIComponent(tweetStr);
-				
-				xhr.onreadystatechange = function() {
-					
-					console.log("state: " + xhr.readyState + " status: " + xhr.status);
-					
-					if (xhr.readyState==4 && xhr.status==200)
-				    {
-						console.log(xhr.response);												
-				    }
-				};
-				
-				xhr.open("POST", url+'?'+body, true);
 				
 				var nonce = TwitterOAuth.generateNonce(32);
 				var ts = String(Math.floor(Date.now() / 1000));
@@ -50,37 +37,18 @@ TwitterRequests.postReply = function(tweetStr)
 					    "oauth_version": "1.0",
 					  };
 					
-				var baseString = TwitterOAuth.getBaseString("POST", url, params);
-
-				var consumerSecret = "vffQZeOOM2bGbsKZijwAIis0wK1RSTsffS6uyL7yPxZtXalxq1";
-				var sig = calcHMAC(baseString, consumerSecret + "&" + response.oauth_token_secret);
-				console.log("signature: " + sig);
+				var sig = TwitterOAuth.createOAuthSignature(url, params, response.oauth_token_secret);
+				
 				params1["oauth_signature"] = sig;
+				var auth = TwitterOAuth.createAuthorizationHeader(params1);	
 
-				var auth = "OAuth ";
-				var keys = Object.keys(params1);
-				for (var i = 0; i < keys.length; ++i) {
-				    var key = keys[i];
-				    auth += (i == 0 ? "" : ", ") + key + "=\"" + encodeURIComponent(params1[key]) + "\"";
-				}
-				console.log("auth header: " + auth);	
-
-				//var userAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.99 Safari/537.36";			
-				
-			    //xhr.setRequestHeader("User-Agent", userAgent);
-			    //xhr.setRequestHeader("HOST", "api.twitter.com");
-				xhr.setRequestHeader("Accept", "*/*");
-				xhr.setRequestHeader("Authorization", auth);
-				//xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
-				
-				
-				//var body = JSON.stringify({status: encodeURIComponent(tweetStr)});
-				
-				console.log("command line:\n" + "curl --request 'POST' 'https://api.twitter.com/1.1/statuses/update.json' --header 'Authorization: " + auth + "' " + body);
-				//xhr.send(body);
-				//console.log("command line:\n" + "curl --request 'POST' 'https://api.twitter.com/oauth/access_token' --header 'Authorization: " + auth + "' ");
-				xhr.send();
+				TwitterOAuth.sendRequest(url+'?'+body, TwitterRequests.handleResponse, auth);
 			}
 		});
 	}
+}
+
+TwitterRequests.handleResponse = function(response)
+{
+	
 }
