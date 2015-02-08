73f423c641f8941addf253147bbbde5dbef3434f
adunofaiur <matthew21573@gmail.com>
2015-02-08 17:31:25 -0600

    Whoops, deleting extra stuff.

--- extractionExtension/code/background.js
+++
@@ -1,5 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  		chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"});
-	});
-});
