09a3a9d6358a505258e159a30c246fbbd934b0aa
adunofaiur <matthew21573@gmail.com>
2015-02-11 13:36:32 -0600

    houseMICE checks for localHost again

--- houseMICE/js/mdc.js
+++ houseMICE/js/mdc.js
@@ -121,7 +121,6 @@ function showMetadata()
 	  var hostname = window.location.hostname;
 	  var port = window.location.port;
 	  SEMANTIC_SERVICE_URL = "http://" + hostname + ":" + port + "/BigSemanticsService/";
-	   SEMANTIC_SERVICE_URL = "http://ecology-service.cse.tamu.edu/BigSemanticsService/";
 
   }
   else{
