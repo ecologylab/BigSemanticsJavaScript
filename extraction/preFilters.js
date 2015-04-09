b4f4882931269793175a59eece60783ad7bf0768
keithkade <keithkade@aol.com>
2015-04-09 17:01:20 -0500

    strip param in prefilter

--- extraction/preFilters.js
+++ extraction/preFilters.js
@@ -8,13 +8,14 @@
 var PreFilter = {};
 
 PreFilter.filter = function(location, filterObj){
+	var newLocation = location;
 	
 	for (var p in filterObj.param_ops){
 		var filterOp = filterObj.param_ops[p];
 		//TODO execute the filter
 			
 		if (filterOp.strip_param){
-			//strip
+			newLocation = stripParam(location, filterOp.strip_param.name);
 		}
 		
 		if (filterOp.set_param){
@@ -23,5 +24,28 @@ PreFilter.filter = function(location, filterObj){
 		
 	}
 	
-	return location;
+	return newLocation;
 };
+
+//source: http://stackoverflow.com/questions/16941104/remove-a-parameter-to-the-url-with-javascript
+function stripParam(url, key){
+	var stripped = url.split("?")[0];
+	
+	var param;
+    var params = [];
+    var queryString = (url.indexOf("?") !== -1) ? url.split("?")[1] : "";
+	
+    if (queryString !== "") {
+        params = queryString.split("&");
+        for (var i = params.length - 1; i >= 0; i -= 1) {
+            param = params[i].split("=")[0];
+            if (param === key) {
+                params.splice(i, 1);
+            }
+        }
+		if (params.length > 0){
+        	stripped = stripped + "?" + params.join("&");
+		}
+    }
+    return stripped;
+}
