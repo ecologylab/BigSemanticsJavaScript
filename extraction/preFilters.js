e6002a8d602b461a8a392f50d1705684229ed3da
keithkade <keithkade@aol.com>
2015-04-13 13:20:48 -0500

    added strip_params_but support

--- extraction/preFilters.js
+++ extraction/preFilters.js
@@ -1,6 +1,5 @@
 /**
- * Semantics that transforms locations, with set_param, strip_param semantic actions inside it, 
- * for managing variability in Document location ParsedURL arguments.
+ * Semantics that transforms locations for managing variability in Document location ParsedURL arguments.
  * 
  * @author kade
  */
@@ -19,12 +18,7 @@ PreFilter.filter = function(location, filterObj){
 		}
 		else if (filterOp.set_param){
 			newLocation = setParam(newLocation, filterOp.set_param.name, filterOp.set_param.value);
-		}
-
-		//strip_params_but ?
-		//override params
-		// https://github.com/ecologylab/BigSemanticsWrapperRepository/blob/65886425b35b4ecfe5e9a7a4e8b49e4a933a324b/BigSemanticsWrappers/repository/search/googleSearch.xml
-		
+		}	
 	}
 	
 	if (filterObj.regex){
@@ -32,6 +26,13 @@ PreFilter.filter = function(location, filterObj){
 		var replace = filterObj.regex.replace;
 		newLocation = newLocation.replace(regex,replace);
 	}
+	
+	
+	if (filterObj.strip_params_but){
+		newLocation = stripParamsBut(newLocation, filterObj.strip_params_but);
+	}
+	
+	//if filterObj.overide_params
 	
 	return newLocation;
 };
@@ -48,6 +49,35 @@ function stripParam(url, name){
         for (var i = params.length - 1; i >= 0; i -= 1) {
             param = params[i].split("=")[0];
             if (param === name) {
+                params.splice(i, 1);
+            }
+        }
+		if (params.length > 0){
+        	stripped = stripped + "?" + params.join("&");
+		}
+    }
+    return stripped;
+}
+
+function stripParamsBut(url, keepParams){
+	var stripped = url.split("?")[0];
+	var param;
+    var params = [];
+    var queryString = (url.indexOf("?") !== -1) ? url.split("?")[1] : "";
+	
+    if (queryString !== "") {
+        params = queryString.split("&");
+        for (var i = params.length - 1; i >= 0; i -= 1) {
+            param = params[i].split("=")[0];
+            
+			var keep = false;
+			for (var p in keepParams){
+				if (param === keepParams[p]) {
+					keep = true;
+				}
+			}
+			
+			if (!keep) {
                 params.splice(i, 1);
             }
         }
