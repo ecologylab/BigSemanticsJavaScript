1a8ba0e750db9d09e584c64a69f8ef62dabfd2d6
Yin Qu <me.yin.qu@gmail.com>
2016-05-31 04:06:48 -0500

    Minor change to Readyable.d.ts

--- bsjsCore/Readyable.d.ts
+++ bsjsCore/Readyable.d.ts
@@ -1,12 +1,19 @@
 // Readyable type definition.
 
-export declare class Readyable {
+export interface IReadyable {
+  isReady(): boolean;
+  onReady(callback: (err: Error, that: this)=>void): void;
+  setReady(): void;
+  setError(err: Error): void;
+}
+
+export declare class Readyable implements IReadyable {
   private ready: boolean;
   private callbackQueue: Array<Function>;
   private error: Error;
 
   isReady(): boolean;
-  onReady(callback: (err: Error, that: Readyable)=>void): void;
+  onReady(callback: (err: Error, that: this)=>void): void;
   setReady(): void;
   setError(err: Error): void;
 }
